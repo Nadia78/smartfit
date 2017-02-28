b054b28cc29ae2bbd87c8741c4482f6780a3b61c
Nadia78 <environnement.eau@gmail.com>
2017-02-28 17:58:47 +0100

    front-end connexion.php

---
+++ connexion.php
@@ -0,0 +1,41 @@
+ <?php include 'header.php';
+?> 
+
+   <div class="formConnexion col-md-4 col-md-offset-4">
+    		
+	    	<form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+			
+			  <div class="form-group">
+			    <label for="email">Email</label>
+			    <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+			  </div>
+
+			 	<?php if(isset($errors['email'])): ?>
+	    			<span id="helpBlock2" class="help-block"> 
+	    				<?php echo $errors['email'] ;?> 
+	    			</span>
+		    	<?php endif;?>
+
+			  <div class="form-group">
+			    <label for="password">Mot de passe</label>
+			    <input type="password" class="form-control" name="password" id="password" placeholder="Mot de passe">
+			  </div>
+
+    		<?php if(isset($errors['password'])) : ?> 
+    			<span id="helpBlock2" class="help-block">
+    				<?php echo ($errors['password']);?> 
+    			</span>
+    		<?php endif;?>
+  	
+			  <button type="submit" name="action" class="btn btn-primary">Valider</button>
+               <div class="form-group">
+                    <p class="help-block"><a href="forgot_password.php">Mot de passe oublié?</a></p>
+                </div>
+			
+			</form>
+    </div>
+	<div>
+		<p> mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+	</div>
+<?php include 'footer.php';
+?>

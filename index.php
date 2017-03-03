1351f959732420c02d95f7429c88c6d29324a39e
Nadia78 <environnement.eau@gmail.com>
2017-03-03 09:04:20 +0100

    got back lost datas

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include 'header.php';
 					<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
 				</div>
 				 <div class="form-group">
-					<label for="exampleInputPassword1">Tranche d'Ã¢ge</label>
+					<label for="exampleInputPassword1">Tranche d'âge</label>
 					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
 				</div>
 				<div class="form-group">
@@ -30,7 +30,7 @@ include 'header.php';
 					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
 				</div>
 				<div class="form-group">
-					<label for="exampleInputPassword1">ActivitÃ©s</label>
+					<label for="exampleInputPassword1">Activités</label>
 					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
 				</div>
 				<div class="form-group">

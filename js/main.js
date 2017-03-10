e4790411e30056b98d1400619c7066263b8aa365
Nadia78 <environnement.eau@gmail.com>
2017-03-10 13:38:18 +0100

    program code js

--- js/main.js
+++ js/main.js
@@ -31,37 +31,38 @@ function AfficheLaDate(){
 //*****************Calcul automatique de la r?partition des proportions ***********************************************************************
 
 //1. R?cup?rer les derni?res valeurs saisies
+
 function recupVal(id){
 	
+	var tabValeursSaisies=[];
 	if(document.getElementById(id).value){
-		var tabValeursSaisies=[];
+		
 			var valeursSaisies=document.getElementById(id).value;
 			
 			console.log(valeursSaisies);
-		
-				tabValeursSaisies.push(valeursSaisies);
-				
+				tabValeursSaisies.push(valeursSaisies);					
 	}
-	console.log(tabValeursSaisies);	
+	return tabValeursSaisies;
+	console.log(tabValeursSaisies);
 }
 
 //2. stocker les valeurs dans un tableau ? 4 ?l?ments et r?cup?rer les 4 derniers ?l?ments pour faire le calcul
 
 
 //3. Effectuer le calcul de la r?partiion en fonction des valeurs saisies allant jusqu'? 4 et conserver les derni?res valeurs
-function CalculRepartAliments(){
+function CalculRepartAliments(id){
 
-/* 	if(liste1!=null || liste1!="undefined"){
-		document.getElementById('liste2').value=(100-document.getElementById('liste1').value)/3;
-		document.getElementById('liste3').value=(100-document.getElementById('liste1').value)/3;
-		document.getElementById('liste4').value=(100-document.getElementById('liste1').value)/3;
+	if(document.getElementById(id).value){
+		//document.getElementById(id).value=(100-(tabValeursSaisies.length)/(tabValeursSaisies.length);
+		//document.getElementById('liste3').value=(100-document.getElementById('liste1').value)/3;
+		//document.getElementById('liste4').value=(100-document.getElementById('liste1').value)/3;
 	}
-	else if(liste1!=null || liste1!="undefined" && liste2!=null || liste2!="undefined"){
+	//else if(liste1!=null || liste1!="undefined" && liste2!=null || liste2!="undefined"){
 		//document.getElementById('liste3').value=100-(document.getElementById('liste1').value)+(document.getElementById('liste3').value);
-	}
+	//}
 	else{
-
-	} */
+		
+	}
 	
 	// .value=nombre4-(nombre!=0);
 	// var compteur=0; qui quantifie les valeurs diff de 0
@@ -77,3 +78,5 @@ function CalculRepartAliments(){
 		case 
 		case 
 	} */
+//**************************************Programme************************************************************************************************
+

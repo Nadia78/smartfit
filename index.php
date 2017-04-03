df84472f967a68af480ed8d0e19e81d71f36257a
Nadia78 <environnement.eau@gmail.com>
2017-04-03 20:52:08 +0200

    html content paragraph

--- index.php
+++ index.php
@@ -184,7 +184,7 @@ echo <pre>; */
         </article>
         <aside>
             <h3>Le saviez-vous ?</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
+            <p>Les graisses stimulent la muqueuse gastrique qui sécrète une hormone qui stimule la production et la sécrétion d'enzymes digestives et, signale au cerveau que nous avons mangé suffisamment. Les produits laitiers à 0% de matières grasses et autres produits à 0% inhibent ce signal et peuvent vous inciter à trop manger.</p>
         </aside>
 		<div id="result"></div>
     </div> <!-- #main -->

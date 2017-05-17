ff47f499a94ea3ed2806686ab779565c80f2fbba
Nadia78 <environnement.eau@gmail.com>
2017-05-17 16:04:49 +0200

    next code

--- index.php
+++ index.php
@@ -86,8 +86,8 @@ if(isset($_POST['action'])){
 	if(isset($imc)){
 		echo "votre imc est de".$imc;
 	}
-	//2. Calcul de la répartition de l'alimentation
-	$alimentMax=max($liste,$liste2,$liste3,$liste4);
+	//2. Calcul de la répartition de l'alimentation et récupérer les labels ou intitulés des champs
+	$alimentMax=max($liste1,$liste2,$liste3,$liste4);
 	
 	if($alimentMax!=null){
 		echo "L'aliment dominant est :".$alimentMax;
